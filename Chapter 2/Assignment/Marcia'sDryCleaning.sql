17843c5f1861e3be9dee7625f5de4b475b56c6ae
GiftedGiftB <asekhamegift@gmail.com>
2025-08-12 01:10:40 +0100

    Question C

--- Chapter 2/Assignment/Marcia'sDryCleaning.sql
+++ Chapter 2/Assignment/Marcia'sDryCleaning.sql
@@ -11,3 +11,8 @@ FROM invoice_item;
 -- Question B
 select LastName, FirstName, Phone
 from customer;
+
+-- Question C
+select LastName, FirstName, Phone
+from customer
+where FirstName regexp 'Nikki';
